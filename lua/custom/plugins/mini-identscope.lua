49bd4278bcb39bdc2acb83017435499542494202
bryankenote <bryan.kenote@faithlife.com>
2023-11-28 09:34:50 -0800

    add mini.indentscope

---
+++ lua/custom/plugins/mini-identscope.lua
@@ -0,0 +1,14 @@
+return {
+  'echasnovski/mini.indentscope',
+  opts = {
+    symbol = '▎',
+    options = {
+      try_as_border = true,
+    },
+    draw = {
+      animation = function()
+        return 0
+      end,
+    },
+  },
+}
