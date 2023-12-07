978d2df104bd5b265c4b6eabc8ed91c6dd782401
bryankenote <bryan.kenote@faithlife.com>
2023-12-07 14:19:20 -0800

    add icon for fsd files

--- lua/custom/plugins/web-devicons.lua
+++ lua/custom/plugins/web-devicons.lua
@@ -1,1 +1,14 @@
-return { "nvim-tree/nvim-web-devicons" }
+local icons = require 'icons'
+return {
+  'nvim-tree/nvim-web-devicons',
+  opts = {
+    override_by_extension = {
+      ['fsd'] = {
+        icon = icons.ui.BoldGear,
+        color = '#555555',
+        cterm_color = '240',
+        name = 'FSD',
+      },
+    },
+  },
+}
