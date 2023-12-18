be036e6c3fb43e3ade6bf9a62b7c7dab7934c212
bryankenote <bryan.kenote@faithlife.com>
2023-12-18 15:05:17 -0800

    disable dressing for nvim tree

--- lua/custom/plugins/dressing.lua
+++ lua/custom/plugins/dressing.lua
@@ -1,4 +1,15 @@
 return {
   'stevearc/dressing.nvim',
   opts = {},
+  config = function()
+    require('dressing').setup {
+      input = {
+        get_config = function()
+          if vim.api.nvim_buf_get_option(0, 'filetype') == 'NvimTree' then
+            return { enabled = false }
+          end
+        end,
+      },
+    }
+  end,
 }
